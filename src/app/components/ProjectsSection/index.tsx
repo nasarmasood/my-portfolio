5a342c644fc90a622433179a3c11c7ad51214421
Nasar Masood <nasarmasood111@gmail.com>
2025-10-07 09:23:18 +0500

    projects section

---
+++ src/app/components/ProjectsSection/index.tsx
@@ -0,0 +1,121 @@
+import React from 'react'
+
+function ProjectSection() {
+  return (
+    <div className="container-wrapper bg-grey">
+        <div className='container'>
+            <div className='grid grid-cols-1 md:grid-cols-3 gap-5'>
+                <div className='item bg-white shadow-md rounded-lg rounded-2'>
+                    <div className='w-full h-[200px]'>
+                       <img
+              src="/images/projectpic.svg"
+              alt="My Picture"
+              className="w-[100%] h-[100%] object-cover"
+            />
+                    </div>
+                    <div className='border-grey-dark border-1 p-4'>
+                        <p className='font-medium text-[16px] text-grey-dark'>Full Stack</p>
+                        <p className='font-semibold text-[18px]'>zebra Docto</p>
+                        <p className='text-main-text text-[14px] mt-[15px]'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
+                    <div
+                role="button"
+                style={{
+                  border: "2px solid #A53DFF",
+                  width:'165px',
+                  borderRadius: "4px",
+                  color: "#A53DFF",
+                  display:'flex',
+                  gap:'5px'
+                }}
+                className="cursor-pointer  px-[10px] py-[8px] rounded-1 inline-block mt-[20px]"
+              >
+               <p className="font-semibold text-[16px]">Download CV</p>
+                <div className="w-[24px] h-[24px]"> <img
+              src="/images/rightarrow.svg"
+              alt="download icon"
+              className="w-full h-full object-cover"
+            /></div>
+              </div>
+                    </div>
+
+                </div>
+                
+                 <div className='item bg-white shadow-md rounded-lg rounded-2'>
+                    <div className='w-full h-[200px]'>
+                       <img
+              src="/images/projectpic.svg"
+              alt="My Picture"
+              className="w-[100%] h-[100%] object-cover"
+            />
+                    </div>
+                    <div className='border-grey-dark border-1 p-4'>
+                        <p className='font-medium text-[16px] text-grey-dark'>Full Stack</p>
+                        <p className='font-semibold text-[18px]'>zebra Docto</p>
+                        <p className='text-main-text text-[14px] mt-[15px]'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
+                    <div
+                role="button"
+                style={{
+                  border: "2px solid #A53DFF",
+                  width:'165px',
+                  borderRadius: "4px",
+                  color: "#A53DFF",
+                  display:'flex',
+                  gap:'5px'
+                }}
+                className="cursor-pointer  px-[10px] py-[8px] rounded-1 inline-block mt-[20px]"
+              >
+               <p className="font-semibold text-[16px]">Download CV</p>
+                <div className="w-[24px] h-[24px]"> <img
+              src="/images/rightarrow.svg"
+              alt="download icon"
+              className="w-full h-full object-cover"
+            /></div>
+              </div>
+                    </div>
+
+                </div>
+
+                 <div className='item bg-white shadow-md rounded-lg rounded-2'>
+                    <div className='w-full h-[200px]'>
+                       <img
+              src="/images/projectpic.svg"
+              alt="My Picture"
+              className="w-[100%] h-[100%] object-cover"
+            />
+                    </div>
+                    <div className='border-grey-dark border-1 p-4'>
+                        <p className='font-medium text-[16px] text-grey-dark'>Full Stack</p>
+                        <p className='font-semibold text-[18px]'>zebra Docto</p>
+                        <p className='text-main-text text-[14px] mt-[15px]'>Vivamus eleifend convallis ante, non pharetra libero molestie laoreet. Donec id imperdiet lacus.</p>
+                    <div
+                role="button"
+                style={{
+                  border: "2px solid #A53DFF",
+                  width:'165px',
+                  borderRadius: "4px",
+                  color: "#A53DFF",
+                  display:'flex',
+                  gap:'5px'
+                }}
+                className="cursor-pointer  px-[10px] py-[8px] rounded-1 inline-block mt-[20px]"
+              >
+               <p className="font-semibold text-[16px]">Download CV</p>
+                <div className="w-[24px] h-[24px]"> <img
+              src="/images/rightarrow.svg"
+              alt="download icon"
+              className="w-full h-full object-cover"
+            /></div>
+              </div>
+                    </div>
+
+                </div>
+             
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default ProjectSection
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import HeroSection from "./components/HeroSection";
-
+import ProjectSection from "./components/ProjectsSection";
 export default function Home() {
   return (
     <div>
      <HeroSection/>
+     <ProjectSection/>
      
     </div>
   );
